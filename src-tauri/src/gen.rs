5cab22ca19bff76ccc7941c0c773f7f8c03a50e9
Anubhab Bandyopadhyay <4890833+AnubhabB@users.noreply.github.com>
2024-09-18 20:26:37 +0530

    Query generation mechanism

--- src-tauri/src/gen.rs
+++ src-tauri/src/gen.rs
@@ -1,7 +1,7 @@
 use std::path::Path;
 
 use anyhow::{anyhow, Result};
-use candle_core::{quantized::gguf_file, Device};
+use candle_core::{quantized::gguf_file, Device, Tensor};
 use candle_transformers::{
     generation::{LogitsProcessor, Sampling},
     models::quantized_llama::ModelWeights,
@@ -75,9 +75,11 @@ impl Generator {
 
 #[derive(Debug, Deserialize)]
 pub struct QueryMore {
+    #[serde(skip)]
     src: String,
+    #[serde(rename = "sub_queries")]
     more: Vec<String>,
-    topic: Option<String>,
+    topic: String,
 }
 
 impl Generator {
@@ -98,8 +100,91 @@ impl Generator {
     }
 
     /// Preprocesses a query to generate `topic` and supplimental queries for `Fusion Retrieval`
-    pub fn query_preproc(&mut self, query: &str, num_sub_qry: usize) -> Result<()> {
-        let prompt = format!("");
+    pub fn query_preproc(&mut self, query: &str, num_sub_qry: usize) -> Result<QueryMore> {
+        let prompt = format!(
+"<|start_header_id|>system<|end_header_id|>\n\nThis is a query generation system for Fusion Retrieval. It generates relevant sub-queries related to a given source query.<|eot_id|><|start_header_id|>user<|end_header_id|>\n\nGiven a source query that may require additional context or specific information, generate relevant sub-queries to retrieve more accurate results. Identify a word or a very short phrase that represents the topic of the `Query`.\n\n\nSource Query:\n{query}\n\nGenerate {num_sub_qry} relevant sub-queries that:\n\n- Are closely related to the source query\n- Can be used to retrieve additional context or specific information\n- Are concise and clear\n\n\nRequirements:\n\n- Sub-queries should not repeat the source query\n- Sub-queries should be relevant to the source query's intent, purpose and context\n- use natural language\n- your answer should be a valid json of the following schema.\n\n\nSchema:\n\n
+{{
+  sub_queries: Array<string>,
+  topic: string
+}}\n\nAnswer must be a valid json.<|eot_id|><|start_header_id|>assistant<|end_header_id|>\n\n{{\n  \"sub_queries\": [\""
+        );
+
+        let input = self
+            .tokenizer
+            .encode(prompt.as_str(), true)
+            .map_err(|e| anyhow!(e))?;
+
+        println!("Prompt:\n{prompt}\nSize: {}", input.len());
+
+        let mut all_tokens = vec![];
+
+        let mut ip = Tensor::new(input.get_ids(), &self.device)?.unsqueeze(0)?;
+        let mut logits = self.model.forward(&ip, 0)?;
+        let mut next = self.sampler.sample(&logits.squeeze(0)?)?;
+
+        all_tokens.push(next);
+
+        for i in input.len()..MAX_NEW_TOKENS {
+            ip = Tensor::new(&[next], &self.device)?.unsqueeze(0)?;
+
+            logits = self.model.forward(&ip, i)?;
+            next = self.sampler.sample(&logits.squeeze(0)?)?;
+
+            if self.stop_tokens.contains(&next) {
+                break;
+            }
+
+            all_tokens.push(next);
+        }
+
+        let tk = match self.tokenizer.decode(&all_tokens[..], false) {
+            Ok(t) => t,
+            Err(e) => {
+                eprintln!("Error generating tokens: {e:?}");
+                anyhow::bail!("Error generating tokens")
+            }
+        };
+        let tk = format!("{{\n  \"sub_queries\": [\"{tk}");
+        let mut res = serde_json::from_str::<QueryMore>(&tk)?;
+        res.src = query.to_string();
+
+        Ok(res)
+    }
+}
+
+// """You are an AI assistant tasked with breaking down complex queries into simpler sub-queries for a RAG system.
+// Given the original query, decompose it into 2-4 simpler sub-queries that, when answered together, would provide a comprehensive response to the original query.
+
+// Original query: {original_query}
+
+// example: What are the impacts of climate change on the environment?
+
+// Sub-queries:
+// 1. What are the impacts of climate change on biodiversity?
+// 2. How does climate change affect the oceans?
+// 3. What are the effects of climate change on agriculture?
+// 4. What are the impacts of climate change on human health?"""
+
+#[cfg(test)]
+mod tests {
+    use std::path::Path;
+
+    use anyhow::Result;
+
+    use crate::utils::select_device;
+
+    use super::Generator;
+
+    #[test]
+    fn sub_query() -> Result<()> {
+        let mut gen = Generator::new(Path::new("../models"), &select_device()?)?;
+        let res = gen.query_preproc(
+            "What are the impacts of climate change on the environment?",
+            4,
+        )?;
+        println!("{res:#?}");
+        let res = gen.query_preproc("What are the latest news about Iraq?", 4)?;
+        println!("{res:#?}");
         Ok(())
     }
 }
